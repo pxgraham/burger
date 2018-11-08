df850f25c19f0833bba7947e124d084f5020f731
Peyton <41251008+pxgraham@users.noreply.github.com>
2018-11-07 22:46:40 -0700

    updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
-USE burgers_db;
 INSERT INTO burgers (burger_name) VALUES ('Pork Burger');
 INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
