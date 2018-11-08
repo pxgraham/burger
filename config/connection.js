1bb6a7dd776de784e6a2ac82c3addae006bd0e2a
Peyton <41251008+pxgraham@users.noreply.github.com>
2018-11-07 22:00:13 -0700

    updated

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+// if(process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
+// else {
     connection = mysql.createConnection({
         host: "pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,    
@@ -14,7 +14,7 @@ else {
         password: "y6pil75b2lnsij3n",
         database: "yefsifky9belntbs"
     });
-}
+// }
 
 // Make connection.
 connection.connect(function(err) {
